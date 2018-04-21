6bf6c389600605c421328703a7da95a56e5f77e6
Incogneato <wigginsmichaelc@gmail.com>
2018-04-21 09:34:06 -0400

    chat interface

--- chat-front-end/src/App.js
+++ chat-front-end/src/App.js
@@ -6,13 +6,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='stage'>
+          <h1>Chat</h1>
+          <div className='chat-logs'>
+          </div>
+          <input
+            type='text'
+            placeholder='Enter your message...'
+            className='chat-input'
+            />
+            <button className='send'>
+              Send
+            </button>
+        </div>
       </div>
     );
   }
